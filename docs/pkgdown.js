4b9659b689f8046f0769bae6be7ce3a7cdf0fbf1
Forest Fang <sen.fang@airbnb.com>
2017-06-16 13:57:15 -0700

    rebuild with examples bundling

--- docs/pkgdown.js
+++ docs/pkgdown.js
@@ -1,4 +1,5 @@
 (function() {
+  // use noConflict to avoid dependency conflict with htmlwidgets used in examples
   var jQuery = $.noConflict(false);
   jQuery(function() {
     jQuery("#sidebar").stick_in_parent({offset_top: 40});
